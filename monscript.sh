03a9f0c1177e01c1275799f815f6cf10a01dc9d4
kacimimoghite <moghitkacimi@gmail.com>
2016-01-29 18:32:17 +0100

    teste de generation

--- monscript.sh
+++ monscript.sh
@@ -3,14 +3,15 @@
 #configuration
 git config --global user.email "moghitkacimi@gmail.com"
 git config --global user.name "kacimimoghite"
+
 #using token clone gh-pages branch
-  git clone --quiet --branch=doc https://${GH_TOKEN}@github.com/testHtmlGenerator.git  doc > /dev/null
+ git clone --quiet --branch=doc https://${GH_TOKEN}@github.com/testHtmlGenerator.git  doc > /dev/null
 
 
-  #add, commit and push files
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to doc"
-  git push -fq origin doc > /dev/null
-  echo -e "Deploy completed\n"
+ #add, commit and push files
+ git add -f .
+ git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to doc"
+ git push -fq origin doc > /dev/null
+ echo -e "Deploy completed\n"
 
-  fi
+ #fi

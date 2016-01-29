c8b8fa5ce48ab20277e8646581e9457570b4e1ab
aminesabri <med.amine.sabri@gmail.com>
2016-01-30 00:33:28 +0100

    tests

--- monscript.sh
+++ monscript.sh
@@ -5,14 +5,14 @@ git config --global user.email "moghitkacimi@gmail.com"
 git config --global user.name "kacimimoghite"
 
 #using token clone gh-pages branch
- git clone --quiet --branch=master https://${GH_TOKEN}@github.com/${GH_REF}  doc > /master/null
+ git clone --quiet --branch=master https://${GH_TOKEN}@github.com/${GH_REF}  master > /dev/null
 
 
 
  #add, commit and push files
  git add -f .
  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
- git push -fq origin doc > /master/null
+ git push -fq origin master > /dev/null
  echo -e "Deploy completed\n"
 
  #fi

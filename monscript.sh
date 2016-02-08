e7682f31ec6c09574d3f9e2d11e6951314faf920
kacimimoghite <moghitkacimi@gmail.com>
2016-02-08 21:58:32 +0100

    modif

--- monscript.sh
+++ monscript.sh
@@ -4,15 +4,15 @@
 git config --global user.email "moghitkacimi@gmail.com"
 git config --global user.name "kacimimoghite"
 
- git clone --quiet https://${GH_TOKEN}@github.com/${GH_REF}  master > /dev/null
+#git clone --quiet https://${GH_TOKEN}@github.com/${GH_REF}  master > /dev/null
 
 
 
  #add, commit and push files
- git add -f .
+ git add -f faq.html.
  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
  #git push -fq origin master > /dev/null
- git push --force --quiet https://${GH_TOKEN}@github.com/${GH_REF} origin master > /dev/null 2>&1
+ git push --force --quiet https://${GH_TOKEN}@github.com/${GH_REF} master:gh-pages > /dev/null 2>&1
 
 
  echo -e "Deploy completed\n"

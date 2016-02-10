a0d58d1f10130da4c834c09510929a8a4d953a96
kacimimoghite <moghitkacimi@gmail.com>
2016-02-10 07:22:38 -0800

    modifié

--- monscript.sh
+++ monscript.sh
@@ -11,7 +11,8 @@ git config --global user.name "kacimimoghite"
  #add, commit and push files
  git add -f faq.html
  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
- git push -fq origin master > /dev/null
+ #git push -fq origin master > /dev/null
+ git push -f -q https://${GH_TOKEN}@${GH_REF} master > /dev/null
  #git push --force --quiet https://${GH_TOKEN}@github.com/${GH_REF} origin master > /dev/null 2>&1
 
 

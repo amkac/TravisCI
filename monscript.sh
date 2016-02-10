9251f0a7c7cc8dbf81fc401baae214bbfbe9fd6f
kacimimoghite <moghitkacimi@gmail.com>
2016-02-10 17:19:33 +0100

    fkjfhfdh

--- monscript.sh
+++ monscript.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
 #configuration
+cd $HOME
 git config --global user.email "moghitkacimi@gmail.com"
 git config --global user.name "kacimimoghite"
 
-#git clone --quiet https://${GH_TOKEN}@github.com/${GH_REF}  master > /dev/null
+git clone --quiet https://${GH_TOKEN}@github.com/${GH_REF}  gh-pages > /dev/null
 
+cd gh-pages
+cp $HOME/faq.html ./index.html
 
 
  #add, commit and push files
- git add -f faq.html
+ git add -f .
  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
  #git push -fq origin master > /dev/null
- git push -f -q https://${GH_TOKEN}@github.com/${GH_REF} master > /dev/null
+ git push -f -q https://${GH_TOKEN}@github.com/${GH_REF} gh-pages > /dev/null
  #git push --force --quiet https://${GH_TOKEN}@github.com/${GH_REF} origin master > /dev/null 2>&1
 
 

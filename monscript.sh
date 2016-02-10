e91282440b0193f76d1421de6b6a626dc0313360
kacimimoghite <moghitkacimi@gmail.com>
2016-02-10 19:20:05 +0100

    jjdd

--- monscript.sh
+++ monscript.sh
@@ -22,7 +22,7 @@ ls -al index.html
  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages "
  #git push -fq origin master > /dev/null
  #git push -f -q https://${GH_TOKEN}@github.com/${GH_REF} origin gh-pages > /dev/null
- git push -f -q https://${GH_TOKEN}@github.com/${GH_REF} origin master > /dev/null
+ git push -f -q https://${GH_TOKEN}@github.com/${GH_REF}  master > /dev/null
 
 
 

ca2882d68282705f09889e0bb6ceebe26bf6e309
kacimimoghite <moghitkacimi@gmail.com>
2016-02-10 18:43:36 +0100

    modification

--- monscript.sh
+++ monscript.sh
@@ -16,9 +16,9 @@ cp $HOME/html/faq.html ./index.html
 
  #add, commit and push files
  git add -f .
- git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master "
+ git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages "
  #git push -fq origin master > /dev/null
- git push -f -q https://${GH_TOKEN}@github.com/${GH_REF} origin master > /dev/null
+ git push -f -q https://${GH_TOKEN}@github.com/${GH_REF} origin gh-pages > /dev/null
  #git push --force --quiet https://${GH_TOKEN}@github.com/${GH_REF} origin master > /dev/null 2>&1
 
 

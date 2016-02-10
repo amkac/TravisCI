38e68981941ee33c0001ae85d1b79e1af3c66c2f
kacimimoghite <moghitkacimi@gmail.com>
2016-02-10 22:51:54 +0100

    modif

--- monscript.sh
+++ monscript.sh
@@ -13,8 +13,9 @@ git clone --quiet https://${GH_TOKEN}@github.com/${GH_REF}  gh-pages > /dev/null
 
 cd gh-pages
 #cd master
+ls
 cp $HOME/html/faq.html ./index.html
-ls -a
+
 
 
  #add, commit and push files

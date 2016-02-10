0d06d189c89dbee6ae2345eac3a2c46e67267960
kacimimoghite <moghitkacimi@gmail.com>
2016-02-10 19:43:21 +0100

    Update monscript.sh

--- monscript.sh
+++ monscript.sh
@@ -8,11 +8,11 @@ git config --global user.name "kacimimoghite"
 git config --global push.default simple
 
 #clone branch gh-pages
-#git clone --quiet https://${GH_TOKEN}@github.com/${GH_REF}  gh-pages > /dev/null
-git clone --quiet https://${GH_TOKEN}@github.com/${GH_REF}  master > /dev/null
+git clone --quiet https://${GH_TOKEN}@github.com/${GH_REF}  gh-pages > /dev/null
+#git clone --quiet https://${GH_TOKEN}@github.com/${GH_REF}  master > /dev/null
 
-#cd gh-pages
-cd master
+cd gh-pages
+#cd master
 cp $HOME/html/faq.html ./index.html
 ls -al index.html
 
@@ -21,8 +21,8 @@ ls -al index.html
  git add -f .
  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages "
  #git push -fq origin master > /dev/null
- #git push -f -q https://${GH_TOKEN}@github.com/${GH_REF} origin gh-pages > /dev/null
- git push -f -q https://${GH_TOKEN}@github.com/${GH_REF}  master > /dev/null
+ git push -f -q https://${GH_TOKEN}@github.com/${GH_REF}  gh-pages > /dev/null
+ #git push -f -q https://${GH_TOKEN}@github.com/${GH_REF}  master > /dev/null
 
 
 

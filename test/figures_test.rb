f7b1a24b83b329307ca1d1bb8789ff8927199b3e
kacimimoghite <moghitkacimi@gmail.com>
2016-03-23 21:53:40 +0100

    update figures_test.rb

--- test/figures_test.rb
+++ test/figures_test.rb
@@ -4,7 +4,7 @@ require 'minitest/autorun'
 #ToDo: do that for evey figure in the html file!
 describe File do
   it "should exist in the {images} directory" do
-    assert_equal File.file?('images/88x31.png'), true
+    assert_equal File.file?('images/88x3155.png'), true
   end
 end
 re = Regexp.new("\.html$") # asciidoc source file

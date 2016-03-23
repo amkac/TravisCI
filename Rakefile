46f7ead00c29fd635038d85fc7ae4eb861321a72
kacimimoghite <moghitkacimi@gmail.com>
2016-03-23 17:10:08 +0100

    update test/figures_test

--- Rakefile
+++ Rakefile
@@ -3,11 +3,11 @@ require 'rake/testtask'
 #---------------------------------------------------
 # Launching tests
 #---------------------------------------------------
-#Rake::TestTask.new do |t|
-#  t.libs.push "lib"
-#  t.test_files = FileList['test/*_test.rb']
-#  t.verbose = true
-#end
+Rake::TestTask.new do |t|
+  t.libs.push "lib"
+  t.test_files = FileList['test/*_test.rb']
+  t.verbose = true
+end
 
 #---------------------------------------------------
 # Checkin URLs
--- test/figures_test.rb
+++ test/figures_test.rb
@@ -7,3 +7,27 @@ describe File do
     assert_equal File.file?('images/88x31.png'), true
   end
 end
+re = Regexp.new("\.html$") # asciidoc source file
+
+dir = Dir.new('C:\Users\moghit\GitHub\IntegrationContinue\Html')
+dir.each  {|fn| 
+    if ( fn =~ re ) then
+        print "asciidoc source : " + fn + "\n"
+        paths = []
+        # find all image: or image:: asciidoc macros
+        File.open(fn) { |f|
+          p 'opening ' << fn
+            content = f.read
+            paths = content.scan(/<img src="([^"]*)"/)
+        }
+        imagesDir='C:\Users\moghit\GitHub\IntegrationContinue\images'
+        # test that path is a file
+        paths.flatten.each {|path|
+          describe File do
+            it "should exist in the {images} directory" do
+              assert_equal File.file?(path), true
+            end
+          end
+        }
+    end
+}

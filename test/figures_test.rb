594a80acec4b964bfd471df03c77c5417328db00
kacimimoghite <moghitkacimi@gmail.com>
2016-03-23 21:50:03 +0100

    update figures_test.rb

--- test/figures_test.rb
+++ test/figures_test.rb
@@ -9,25 +9,3 @@ describe File do
 end
 re = Regexp.new("\.html$") # asciidoc source file
 
-dir = Dir.new('/Html')
-dir.each  {|fn| 
-    if ( fn =~ re ) then
-        print "asciidoc source : " + fn + "\n"
-        paths = []
-        # find all image: or image:: asciidoc macros
-        File.open(fn) { |f|
-          p 'opening ' << fn
-            content = f.read
-            paths = content.scan(/<img src="([^"]*)"/)
-        }
-        imagesDir='/images'
-        # test that path is a file
-        paths.flatten.each {|path|
-          describe File do
-            it "should exist in the {images} directory" do
-              assert_equal File.file?(path), true
-            end
-          end
-        }
-    end
-}
